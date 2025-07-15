39389bf541e275a53a7d07e9930f15727a6ed5dd
Shreyas Bakare <56884503+ShreyasBakare@users.noreply.github.com>
2025-07-16 01:34:01 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import { BlogPostCard } from "./components/BlogPostCard"
 import { Navigation } from "./components/Navigation"
+import React from "react";
+import Layout from "./components/Layout";
 
 function App() {
   return (
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <div className="min-h-svh flex flex-col">
-          <Navigation />
-          <div className="flex-1 flex flex-col items-center justify-center">
-            {/* <Avatar>
-              <AvatarImage src="https://github.com/shreyasbakare.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <Button>Click me!</Button> */}
-            <BlogPostCard title="Quantum Entanglement" description="This article is about quantum entanglement. This article is about quantum entanglement"
-             avatarImageLink="https://github.com/shreyasbakare.png" date="2025-01-01" />
-            <BlogPostCard title="String Theory" description="This article is about string theory. This article is about string theory"
-             avatarImageLink="https://github.com/torvalds.png" date="2025-01-01" />
-            {/* <ModeToggle /> */}
+        <Layout>
+          <div className="min-h-svh flex flex-col">
+            <Navigation />
+            <div className="flex-1 flex flex-col items-center justify-center">
+              {/* <Avatar>
+                <AvatarImage src="https://github.com/shreyasbakare.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <Button>Click me!</Button> */}
+              <BlogPostCard title="Quantum Entanglement" description="This article is about quantum entanglement. This article is about quantum entanglement"
+               avatarImageLink="https://github.com/shreyasbakare.png" date="2025-01-01" />
+              <BlogPostCard title="String Theory" description="This article is about string theory. This article is about string theory"
+               avatarImageLink="https://github.com/torvalds.png" date="2025-01-01" />
+              {/* <ModeToggle /> */}
+            </div>
           </div>
-        </div>
+          <h1>Welcome to Your Professional Website</h1>
+          <p>This is your homepage. Add more sections as needed.</p>
+        </Layout>
     </ThemeProvider>
   )
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Container, Box } from "@mui/material";
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">Your Brand</Typography>
+      </Toolbar>
+    </AppBar>
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      {children}
+    </Container>
+    <Box component="footer" sx={{ py: 2, textAlign: "center", bgcolor: "#f5f5f5" }}>
+      <Typography variant="body2">© {new Date().getFullYear()} Your Brand. All rights reserved.</Typography>
+    </Box>
+  </>
+);
+
+export default Layout;
